6ec44fc5992a793502bfa22c68373a4d2ef539ec
azka-rafif <azka.rafif@evermos.com>
2023-08-08 16:56:34 +0700

    get cart

---
+++ internal/domain/cart/cart_service.go
@@ -0,0 +1,60 @@
+package cart
+
+import (
+	"github.com/evermos/boilerplate-go/internal/domain/product"
+	"github.com/evermos/boilerplate-go/shared/failure"
+	"github.com/gofrs/uuid"
+)
+
+type CartService interface {
+	AddToCart(load CartItemPayload, userId, cartId uuid.UUID) (res CartItem, err error)
+	GetCart(cartId uuid.UUID) (res Cart, err error)
+}
+
+type CartServiceImpl struct {
+	Repo           CartRepository
+	ProductService product.ProductService
+}
+
+func ProvideCartServiceImpl(repo CartRepository, proService product.ProductService) *CartServiceImpl {
+	return &CartServiceImpl{Repo: repo, ProductService: proService}
+}
+
+func (s *CartServiceImpl) AddToCart(load CartItemPayload, userId, cartId uuid.UUID) (res CartItem, err error) {
+	exists, err := s.Repo.CartExistsByID(cartId.String())
+
+	if err != nil {
+		return
+	}
+
+	if !exists {
+		err = failure.NotFound("Cart")
+		return
+	}
+
+	// prodId, err := uuid.FromString(load.ProductId.String())
+	// if err != nil {
+	// 	return
+	// }
+	// prod, err := s.ProductService.GetByID(prodId)
+	return
+}
+
+func (s *CartServiceImpl) GetCart(cartId uuid.UUID) (res Cart, err error) {
+	exists, err := s.Repo.CartExistsByID(cartId.String())
+
+	if err != nil {
+		return
+	}
+
+	if !exists {
+		err = failure.NotFound("Cart")
+		return
+	}
+	res, err = s.Repo.GetCartByID(cartId.String())
+	if err != nil {
+		return
+	}
+
+	return
+}
