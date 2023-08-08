ab806a3b24633105e43dd4b20c2ed33ea3d38fa4
azka-rafif <azka.rafif@evermos.com>
2023-08-08 16:57:17 +0700

    get all get by id create

---
+++ internal/domain/product/product_service.go
@@ -0,0 +1,56 @@
+package product
+
+import (
+	"github.com/evermos/boilerplate-go/shared/failure"
+	"github.com/gofrs/uuid"
+)
+
+type ProductService interface {
+	Create(load ProductPayload, userId uuid.UUID) (res Product, err error)
+	GetAll(limit, offset int, sort, field, productTitle string) (res []Product, err error)
+	GetByID(id uuid.UUID) (res Product, err error)
+}
+
+type ProductServiceImpl struct {
+	Repo ProductRepository
+}
+
+func ProvideProductServiceImpl(repo ProductRepository) *ProductServiceImpl {
+	return &ProductServiceImpl{Repo: repo}
+}
+
+func (s *ProductServiceImpl) Create(load ProductPayload, userId uuid.UUID) (res Product, err error) {
+
+	res, err = res.NewFromPayload(load, userId)
+	if err != nil {
+		return
+	}
+	err = s.Repo.Create(res)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (s *ProductServiceImpl) GetAll(limit, offset int, sort, field, productTitle string) (res []Product, err error) {
+	res, err = s.Repo.GetAll(limit, offset, sort, field, productTitle)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (s *ProductServiceImpl) GetByID(id uuid.UUID) (res Product, err error) {
+	exists, err := s.Repo.ExistsByID(id.String())
+
+	if err != nil {
+		return
+	}
+
+	if !exists {
+		err = failure.NotFound("Product")
+		return
+	}
+
+	return
+}
