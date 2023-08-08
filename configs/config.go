0757119e2e41f18f110f5b358a9b605e04b0070f
azka-rafif <azka.rafif@evermos.com>
2023-08-08 10:17:57 +0700

    jwt secret confgi

--- configs/config.go
+++ configs/config.go
@@ -19,9 +19,10 @@ type Config struct {
 			Enable           bool     `mapstructure:"ENABLE"`
 			MaxAgeSeconds    int      `mapstructure:"MAX_AGE_SECONDS"`
 		}
-		Name     string `mapstructure:"NAME"`
-		Revision string `mapstructure:"REVISION"`
-		URL      string `mapstructure:"URL"`
+		Name      string `mapstructure:"NAME"`
+		Revision  string `mapstructure:"REVISION"`
+		URL       string `mapstructure:"URL"`
+		JWTSecret string `mapstructure:"JWT_SECRET"`
 	}
 
 	Cache struct {
