3b9592495bf982a47cc27daac08604f1c408e6df
azka-rafif <azka.rafif@evermos.com>
2023-08-09 14:31:52 +0700

    checkout not done

--- internal/domain/cart/cart_service.go
+++ internal/domain/cart/cart_service.go
@@ -1,6 +1,9 @@
 package cart
 
 import (
+	"errors"
+
+	"github.com/evermos/boilerplate-go/internal/domain/order"
 	"github.com/evermos/boilerplate-go/internal/domain/product"
 	"github.com/evermos/boilerplate-go/shared/failure"
 	"github.com/gofrs/uuid"
@@ -9,15 +12,17 @@ import (
 type CartService interface {
 	AddToCart(load CartItemPayload, userId, cartId uuid.UUID) (res CartItem, err error)
 	GetCart(cartId uuid.UUID) (res Cart, err error)
+	Checkout(load CheckoutPayload, cartId, userId uuid.UUID) (res order.Order, err error)
 }
 
 type CartServiceImpl struct {
 	Repo           CartRepository
 	ProductService product.ProductService
+	OrderService   order.OrderService
 }
 
-func ProvideCartServiceImpl(repo CartRepository, proService product.ProductService) *CartServiceImpl {
-	return &CartServiceImpl{Repo: repo, ProductService: proService}
+func ProvideCartServiceImpl(repo CartRepository, proService product.ProductService, ordService order.OrderService) *CartServiceImpl {
+	return &CartServiceImpl{Repo: repo, ProductService: proService, OrderService: ordService}
 }
 
 func (s *CartServiceImpl) AddToCart(load CartItemPayload, userId, cartId uuid.UUID) (res CartItem, err error) {
@@ -29,13 +34,12 @@ func (s *CartServiceImpl) AddToCart(load CartItemPayload, userId, cartId uuid.UU
 		err = failure.NotFound("Cart")
 		return
 	}
-
-	prodId, err := uuid.FromString(load.ProductId.String())
+	prod, err := s.ProductService.GetByID(load.ProductId)
 	if err != nil {
 		return
 	}
-	prod, err := s.ProductService.GetByID(prodId)
-	if err != nil {
+	if prod.Stock < load.Quantity {
+		err = failure.BadRequest(errors.New("not enough stock available"))
 		return
 	}
 	res, err = res.NewFromPayload(load, cartId, userId, prod.Price)
@@ -77,6 +81,28 @@ func (s *CartServiceImpl) GetCartItems(cartId uuid.UUID) (res []CartItem, err er
 	res, err = s.Repo.GetCartItems(cartId.String())
 	if err != nil {
 		return
+	}
+	return
+}
+
+func (s *CartServiceImpl) Checkout(load CheckoutPayload, cartId, userId uuid.UUID) (res order.Order, err error) {
+	exists, err := s.Repo.CartExistsByID(cartId.String())
+	if err != nil {
+		return
+	}
+	if !exists {
+		err = failure.NotFound("Cart")
+		return
+	}
+	for _, id := range load.CartItemsIds {
+		exists, err = s.Repo.CartItemExistsByID(id)
+		if err != nil {
+			return
+		}
+		if !exists {
+			err = failure.NotFound("Cart item")
+			return
+		}
 	}
 	return
 }
