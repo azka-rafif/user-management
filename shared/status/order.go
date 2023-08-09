00284fc04f14b569b698ae08666fceb4a8c40483
azka-rafif <azka.rafif@evermos.com>
2023-08-09 14:32:46 +0700

    status helper

---
+++ shared/status/order.go
@@ -0,0 +1,37 @@
+package status
+
+import "strings"
+
+type OrderStatus int
+
+const (
+	Pending OrderStatus = iota
+	Paid
+	Shipped
+)
+
+func GetStatusFromString(s string) OrderStatus {
+	switch strings.ToLower(s) {
+	case "pending":
+		return Pending
+	case "paid":
+		return Paid
+	case "shipped":
+		return Shipped
+	default:
+		return Pending
+	}
+}
+
+func GetStringFromStatus(s OrderStatus) string {
+	switch s {
+	case Paid:
+		return "Paid"
+	case Pending:
+		return "Pending"
+	case Shipped:
+		return "Shipped"
+	default:
+		return "Pending"
+	}
+}
