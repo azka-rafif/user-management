8df2c9f3ce4efafd918c818ca500f10ae52d299c
azka-rafif <azka.rafif@evermos.com>
2023-08-10 13:25:43 +0700

    create order and get all

--- internal/domain/order/order_service.go
+++ internal/domain/order/order_service.go
@@ -1,7 +1,11 @@
 package order
 
+import "github.com/gofrs/uuid"
+
 type OrderService interface {
-	Create(load OrderPayload) (res Order, err error)
+	CreateOrder(load OrderPayload, itemLoads []OrderItemPayload) (res Order, err error)
+	CreateOrderItem(load OrderItemPayload) (res OrderItem, err error)
+	GetAll(limit, offset int, sort, field, status string, userId uuid.UUID, userRole string) (res []Order, err error)
 }
 
 type OrderServiceImpl struct {
@@ -12,6 +16,38 @@ func ProvideOrderServiceImpl(repo OrderRepository) *OrderServiceImpl {
 	return &OrderServiceImpl{Repo: repo}
 }
 
-func (s *OrderServiceImpl) Create(load OrderPayload) (res Order, err error) {
+func (s *OrderServiceImpl) CreateOrder(load OrderPayload, itemLoads []OrderItemPayload) (res Order, err error) {
+	res, err = res.NewFromPayload(load)
+	if err != nil {
+		return
+	}
+	items := []OrderItem{}
+	for _, itemLoad := range itemLoads {
+		itemLoad.OrderId = res.Id
+		item, err := s.CreateOrderItem(itemLoad)
+		if err != nil {
+			return res, err
+		}
+		items = append(items, item)
+	}
+	res = res.AttachItems(items)
+	err = s.Repo.Create(res)
+
+	return
+}
+
+func (s *OrderServiceImpl) CreateOrderItem(load OrderItemPayload) (res OrderItem, err error) {
+	res, err = res.NewFromPayload(load)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (s *OrderServiceImpl) GetAll(limit, offset int, sort, field, status string, userId uuid.UUID, userRole string) (res []Order, err error) {
+	res, err = s.Repo.GetAll(limit, offset, sort, field, status, userId.String(), userRole)
+	if err != nil {
+		return
+	}
 	return
 }
